8d85e47360b65dc3321a044ca7ce7499417fd643
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2025-01-28 23:18:46 -0500

    Hubris: revert to old resize method

--- src/renderer/state.rs
+++ src/renderer/state.rs
@@ -343,23 +343,21 @@ impl<'a> State<'a> {
         self.window
     }
 
-    /*
-    x     n_x
-    _ ==   -
-    y     n_y
-
-    x * n_y = n_x * y
-    n_y = (n_x * y) / x
-    */
-
     pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
-        if new_size.width > 0 && new_size.height > 0 {
-            let new_height = (new_size.width * self.config.height) / self.config.width;
-            self.size = new_size;
-            self.size.height = new_height;
+        // if new_size.width > 0 && new_size.height > 0 {
+        //     let new_height = (new_size.width * self.config.height) / self.config.width;
+        //     self.size = new_size;
+        //     self.size.height = new_height;
+        //
+        //     self.config.width = new_size.width;
+        //     self.config.height = new_height;
+        //     self.surface.configure(&self.device, &self.config);
+        // }
 
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
             self.config.width = new_size.width;
-            self.config.height = new_height;
+            self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
         }
     }
